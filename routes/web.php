e818bf8776ff76548bb98e6c1da2c7bde6bf352c
Custódio Junior <crbj2506@gmail.com>
2022-12-12 14:51:43 -0300

    Protegendo a rota /home (verificação e-mail)

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home')
+    ->middleware('verified');
