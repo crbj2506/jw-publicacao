3752b25b9ae22b2a07aa7bc2798d12c3197cbc77
Custódio Junior <crbj2506@gmail.com>
2023-01-06 04:54:42 -0300

    Melhoria - Aumentando Card lista Inventário

--- resources/views/inventario/show.blade.php
+++ resources/views/inventario/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header fs-5">{{ __('Visualizando Inventário da Congregação ') . $congregacao->nome }}</div>
                 <div class="card-body">
