ebb32f9e5f2541f2def85d163521bcd8cbaaf4f5
Custódio Junior <crbj2506@gmail.com>
2023-02-03 05:01:44 -0300

    Atualização CRUD Congregação

--- resources/views/congregacao/crud.blade.php
+++ resources/views/congregacao/crud.blade.php
@@ -5,7 +5,16 @@
     {{--Rota INDEX Lista --}}
     @if(isset($congregacoes))
         <div class="card m-3">
-            <div class="card-header fw-bold">{{ __('Lista de Congregações') }}</div>
+            <div class="card-header fw-bold container-fluid">
+                <div class="row align-items-center">
+                    <div class="col">
+                        {{ __('Lista de Congregações') }}
+                    </div>
+                    <div class="col-4 container-fluid d-flex-inline text-end p-0">
+                            <a href="{{ route('congregacao.create')}}" class="btn btn-sm btn-outline-success py-0 ">Nova</a>
+                    </div>  
+                </div>
+            </div>
             <div class="card-body">
                 <table class="table table-striped table-hover">
                     <thead>
@@ -20,7 +29,7 @@
                         @foreach ($congregacoes as $key => $c)
                             <tr>
                                 <th class="text-center py-0" scope="row">{{$c->id}}</th>
-                                <td class="text-center py-0" scope="row">{{$c->nome}}</td>
+                                <td class="py-0" scope="row">{{$c->nome}}</td>
                                 <td class="text-center py-0" scope="row"><a href="{{ route('congregacao.show',['congregacao' => $c])}}" class="btn btn-sm btn-outline-primary py-0">Ver</a></td>
                                 <td class="text-center py-0" scope="row"><a href="{{ route('congregacao.edit',['congregacao' => $c])}}" class="btn btn-sm btn-outline-warning py-0">Editar</a></td>
                             </tr>
@@ -32,14 +41,19 @@
         </div>
     @else
         <div class="card m-3">
-            <div class="card-header fw-bold container">
-            
-                <div class="row">
+            <div class="card-header fw-bold container-fluid">
+                <div class="row align-items-center">
                     <div class="col">
                         @if(isset($congregacao->edit)) Altera Congregação @elseif(!isset($congregacao->show)) Nova Congregação @elseif(isset($congregacao->show)) Mostra Congregação @endif
                     </div>
-                    <div class="col-3 text-end text-center">
-                        <a href="{{ route('congregacao.index')}}" class="btn btn-sm btn-outline-primary py-0">Listar</a>
+                    <div class="col-5 container-fluid d-flex-inline text-end p-0">
+                            <a href="{{ route('congregacao.index')}}" class="btn btn-sm btn-outline-primary me-2 py-0">Listar</a>
+                            @if(isset($congregacao->edit)) 
+                                <a href="{{ route('pedido.create')}}" class="btn btn-sm btn-outline-success py-0 ">Nova</a>
+                            @elseif(!isset($congregacao->show))
+                            @elseif(isset($congregacao->show))
+                                <a href="{{ route('congregacao.create')}}" class="btn btn-sm btn-outline-success py-0 ">Nova</a>
+                            @endif
                     </div>
                 </div>
             </div>
